d28c9135276ff45411a318208e70d491a3e0f202
Drew Stephens <drew@dinomite.net>
2021-09-06 09:24:05 -0400

    Add GH 498 test

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,12 +8,14 @@ repositories {
 }
 
 dependencies {
-    kotlin("stdlib-jdk8")
-    kotlin("test")
-    kotlin("test-junit")
+    implementation(kotlin("stdlib-jdk8"))
+    implementation(kotlin("test"))
 
-    implementation("com.fasterxml.jackson:jackson-bom:2.12.5")
+    implementation(platform("com.fasterxml.jackson:jackson-bom:2.12.5"))
     implementation("com.fasterxml.jackson.core:jackson-databind")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+
+    testImplementation(kotlin("test-junit"))
 }
 
 application {
---
+++ app/src/test/kotlin/net/dinomite/jackson/test/GitHub498.kt
@@ -0,0 +1,88 @@
+package net.dinomite.jackson.test
+
+import com.fasterxml.jackson.databind.DeserializationFeature
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.KotlinModule
+import com.fasterxml.jackson.module.kotlin.readValue
+import com.fasterxml.jackson.module.kotlin.registerKotlinModule
+import org.junit.Test
+
+class GitHub498 {
+    private val mapper = ObjectMapper()
+        .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+        .registerModule(KotlinModule())
+    val json = """{"A":1,"B":2}"""
+
+    @Test // succeeds
+    fun upperAUpperB() {
+        data class Data(var A: Int, var B: Int)
+        mapper.readValue<Data>(json)
+    }
+
+    @Test // succeeds
+    fun upperALowerB() {
+        data class Data(var A: Int, var b: Int)
+        mapper.readValue<Data>(json)
+    }
+
+    @Test // succeeds
+    fun lowerAUpperB() {
+        data class Data(var a: Int, var B: Int)
+        mapper.readValue<Data>(json)
+    }
+
+    @Test // succeeds
+    fun lowerA() {
+        data class Data(var a: Int)
+        mapper.readValue<Data>(json)
+    }
+
+    @Test // succeeds
+    fun lowerB() {
+        data class Data(var b: Int)
+        mapper.readValue<Data>(json)
+    }
+
+    @Test // fails
+    fun upperA() {
+        data class Data(var A: Int)
+        mapper.readValue<Data>(json)
+    }
+
+    @Test // Fails
+    fun upperB() {
+        data class Data(var B: Int)
+        mapper.readValue<Data>(json)
+    }
+}
+
+class Bar {
+    val mapper = ObjectMapper()
+        .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+        .registerKotlinModule()
+
+    @Test // ok
+    fun lowercaseJsonStringProperty() {
+        val json = """{"abc":"hi"}"""
+
+        data class Data(val abc: String)
+        mapper.readValue<Data>(json)
+    }
+
+    @Test // fails
+    fun uppercaseJsonStringProperty() {
+        val json = """{"Abc":"hi"}"""
+
+        data class Data(val abc: String)
+        mapper.readValue<Data>(json)
+    }
+
+    @Test // ok
+    fun uppercaseJsonIntProperty() {
+        val json = """{"Abc":1}"""
+
+        data class Data(var abc: Int)
+        mapper.readValue<Data>(json)
+    }
+}
+
