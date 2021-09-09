9f4184318d534f482b1d8be7e427214f89bf3463
Drew Stephens <drew@dinomite.net>
2021-09-09 09:05:45 -0400

    Explicitly configure junit platform

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     id("org.jetbrains.kotlin.jvm") version "1.5.30"
-    application
 }
 
 repositories {
@@ -9,11 +8,12 @@ repositories {
 
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
-    implementation(kotlin("test"))
+    implementation(kotlin("reflect"))
 
-    implementation(platform("com.fasterxml.jackson:jackson-bom:2.12.5"))
-    implementation("com.fasterxml.jackson.core:jackson-databind")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.12.5")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.12.5")
 
     testImplementation(kotlin("test-junit5"))
 }
+
+tasks.test { useJUnitPlatform() }
